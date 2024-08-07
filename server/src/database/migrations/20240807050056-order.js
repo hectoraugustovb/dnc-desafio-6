d006b13e70043b741d81fd5cd02d3b283d76074d
Noww01 <hector752220@gmail.com>
2024-08-07 02:06:49 -0300

    Feat: Order migration created

---
+++ server/src/database/migrations/20240807050056-order.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('clients', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'products',
+          key: 'id'
+        }
+      },
+      buyer_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        references: {
+          model: 'clients',
+          key: 'id'
+        }
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+        onUpdate: Sequelize.NOW
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('clients');
+  }
+};
