612ec298519e90a73934691bcfa11960627699a8
hector59a2b9297 <hector.02220x@gmail.com>
2024-12-28 15:45:38 -0300

    Feat: Database changed

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,9 @@
 import app from "./src/app";
+import connection from "./src/database";
 
-app.listen(4444, () => console.log('Server started'));
+app.listen(4444, async () => {
+    await connection.sync()
+        .then(() => console.log('Database connected'));
+
+    console.log('Server started')
+});
--- server/src/database/config/database.js
+++ server/src/database/config/database.js
@@ -1,20 +1,12 @@
+const { AbstractDialect } = require('sequelize/lib/dialects/abstract/index');
 require('dotenv').config();
 
-const db = {
+const dbConfig = {
   host: process.env.DATABASE_HOST,
   port: Number(process.env.DATABASE_PORT),
-  name: process.env.DATABASE_NAME,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD
-}
-
-const dbConfig = {
-  dialect: 'postgres',
-  host: db.host,
-  port: db.port,
-  username: db.user,
-  password: db.password,
-  database: db.name,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   define: {
     underscored: true,
     timestamps: false
